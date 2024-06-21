09ef01cccde3632975e000101f30eb9ad5005639
Ian Dai <ianbbdai@gmail.com>
2024-06-20 20:40:15 -0700

    feat: frontend form sends user and hashed password

--- insertIntoMongo/index.js
+++ insertIntoMongo/index.js
@@ -32,11 +32,11 @@ exports.handler = async (event, context) => {
   /* By default, the callback waits until the runtime event loop is empty before freezing the process and returning the results to the caller. Setting this property to false requests that AWS Lambda freeze the process soon after the callback is invoked, even if there are events in the event loop. AWS Lambda will freeze the process, any state data, and the events in the event loop. Any remaining events in the event loop are processed when the Lambda function is next invoked, if AWS Lambda chooses to use the frozen process. */
   context.callbackWaitsForEmptyEventLoop = false;
   
-  const body = JSON.parse(event.body);
-  return {
-    statusCode: 200,
-    body: "Got " + JSON.stringify(body),
-  };
+  // const body = JSON.parse(event.body);
+  // return {
+  //   statusCode: 200,
+  //   body: "Got " + JSON.stringify(body),
+  // };
 
   // Get an instance of our database
   const db = await connectToDatabase();
@@ -53,6 +53,11 @@ exports.handler = async (event, context) => {
 
   const response = {
     statusCode: 200,
+    headers: {
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+    },
     body: JSON.stringify(result),
   };
 
--- receipts-proj/src/Form.tsx
+++ receipts-proj/src/Form.tsx
@@ -1,34 +1,60 @@
-import { FormEvent } from "react";
+import bcrypt from "bcryptjs";
+import "./Form.css";
 
-const body = {
-  name: "myname",
-};
+// const lambdaURL =
+//   "https://wyo8uceuk3.execute-api.us-east-2.amazonaws.com/default/insert";
+const lambdaURL = "http://localhost:3000/test";
+const saltRounds = 10;
 
-const lambdaURL =
-  "https://wyo8uceuk3.execute-api.us-east-2.amazonaws.com/default/insert";
-
-function handleSubmit(event: FormEvent<HTMLFormElement>) {
+function handleSubmit(event: React.SyntheticEvent) {
   event.preventDefault();
-  fetch(lambdaURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  })
-    .then((response) => response.text())
-    .then((data) => console.log(data))
-    .catch((error) => console.error(error));
+
+  const form = event.currentTarget as HTMLFormElement | undefined;
+  const formData = new FormData(form);
+  const body: {
+    [key: string]: string;
+  } = {};
+  formData.forEach((value, key) => (body[key] = value as string));
+
+  bcrypt.hash(
+    body["password"],
+    saltRounds,
+    function (err: Error | null, hash: string) {
+      body["password"] = hash;
+      fetch(lambdaURL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      })
+        .then((response) => response.text())
+        .then((data) => console.log(data))
+        .catch((error) => console.error(error));
+    }
+  );
 }
 
 export default function Form() {
   return (
-    <form onSubmit={handleSubmit}>
-      <label>First name:</label>
-      <input type="text" id="fname" name="fname" />
-      <label>Last name:</label>
-      <input type="text" id="lname" name="lname" />
-      <button type="submit">Do the thing</button>
-    </form>
+    <div id="formDiv">
+      <form id="submitForm" onSubmit={handleSubmit}>
+        <label>Username:</label>
+        <input type="text" id="username" name="username" />
+        <label>Password:</label>
+        <input type="text" id="password" name="password" />
+        <button type="submit">Do the thing</button>
+      </form>
+      <button
+        onClick={() =>
+          fetch("http://localhost:3000/verify")
+            .then((response) => response.text())
+            .then((data) => console.log(data))
+            .catch((error) => console.error(error))
+        }
+      >
+        Verify
+      </button>
+    </div>
   );
 }
