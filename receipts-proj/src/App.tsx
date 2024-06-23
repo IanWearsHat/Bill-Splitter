909447fe5d70880c52d8b441826d53fc4d4c61d6
Ian Dai <ianbbdai@gmail.com>
2024-06-22 19:24:27 -0700

    Added material UI and created name selector

--- receipts-proj/src/App.tsx
+++ receipts-proj/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Form from "./Form";
+import ReceiptEditor from "./ReceiptEditor";
 
 function App() {
   return (
     <>
-      <Form />
+      {/* <Form /> */}
+      <ReceiptEditor />
     </>
   );
 }
--- receipts-proj/src/Form.tsx
+++ receipts-proj/src/Form.tsx
@@ -1,10 +1,15 @@
 import bcrypt from "bcryptjs";
 import "./Form.css";
 
-const createAccountURL =
-  "https://5xx9atbspi.execute-api.us-east-2.amazonaws.com/default/createAccount";
-const loginURL =
-  "https://5xx9atbspi.execute-api.us-east-2.amazonaws.com/default/login";
+// const createAccountURL =
+//   "https://5xx9atbspi.execute-api.us-east-2.amazonaws.com/default/createAccount";
+// const loginURL =
+//   "https://5xx9atbspi.execute-api.us-east-2.amazonaws.com/default/login";
+const createAccountURL = "http://localhost:3000/createAccount";
+const loginURL = "http://localhost:3000/login";
+
+const createReceiptURL = "http://localhost:3000/createReceipt";
+
 const saltRounds = 10;
 
 function handleSubmit(event: React.SyntheticEvent) {
@@ -56,7 +61,43 @@ function handleLogin(event: React.SyntheticEvent) {
     body: JSON.stringify(body),
   })
     .then((response) => response.text())
-    .then((data) => console.log(data))
+    .then((data) => {
+      console.log(data);
+      localStorage.setItem("token", data);
+    })
+    .catch((error) => console.error(error));
+}
+
+function createReceipt() {
+  const token = localStorage.getItem("token");
+  if (!token) return;
+
+  const data = {
+    token: token,
+    buyers: {
+      Ian: {
+        Apples: 0.6,
+        Oranges: 1.7,
+      },
+      "THE KOOLAID MAN": {
+        Apples: 0.6,
+        Oranges: 1.7,
+      },
+    },
+  };
+  // left off on creating frontend for creating receipts
+
+  fetch(createReceiptURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      console.log(data);
+    })
     .catch((error) => console.error(error));
 }
 
@@ -78,6 +119,8 @@ export default function Form() {
         <input type="text" id="password" name="password" />
         <button type="submit">Login</button>
       </form>
+
+      <button onClick={createReceipt}>Create Receipt</button>
     </div>
   );
 }
